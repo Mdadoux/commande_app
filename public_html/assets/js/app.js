625d8c301e04f984b8379262aebfa34a3de116b8
mDaroueche <m.daroueche@hotmail.fr>
2019-01-01 02:46:59 +0100

    Ajout fonctionnalité calcul des prix selon la quantité du produits

--- public_html/assets/js/app.js
+++ public_html/assets/js/app.js
@@ -2,13 +2,16 @@ $(document).ready(function () {
 
 	//$btnAnnulCommand.hide();
 	check_is_commade();
+	get_items_prices();
 
 	//console.log(userMenu);
 	liste_food_items();
+
 	show_user_menu();
 	$("#liste-ingred li").on('click', function () {
 		var aliment = $(this).attr('class');
 		select_item(aliment);
+		get_items_prices();
 
 	});
 	//récuperer l'éllément à supprimer 
@@ -16,7 +19,28 @@ $(document).ready(function () {
 		var itemToSuppr = $(this).parent().find('.item-content').text();
 		//console.log(itemToSuppr);
 		remove_item_from_menu(itemToSuppr);
+		get_items_prices();
+	});
+
+	//annuler une commande 
+	$(document).on('click', '#annuler-com', function () {
+		if (confirm('voulez vraiment anuler la commande ?')) {
+			cancel_commande();
+			setTimeout(window.location.reload(), 100);
+		}
 	})
+
+	var $quantity = $('.qty');
+
+	$quantity.on('change', function () {
+		get_items_prices();
+
+		if ($(this).val() <= 0) {
+			console.log($(this).val());
+			$(this).value = 0;
+		}
+	})
+
 
 
 
@@ -87,7 +111,6 @@ function show_user_menu() {
 	//var propName = JSON.parse(localStorage.getItem('000'));
 	var prop = Object.values(shopping_cart);
 	//console.log(prop);
-
 	if (userMenu.length > 0) {
 		$listeMenu.show();
 
@@ -96,8 +119,9 @@ function show_user_menu() {
 			const element = prop[i];
 			$listeMenu.append('<li id="' + element.nom + '" class="user_menu-item">\n\
 			<img src="'+ element.image + '"><div class="item-content">' + element.nom + '</div>\n\
-			<div class="item-price">'+ element.price + '<span> €</span></div>\n\
-			<label>quantité :</label><input type="number" class="qty" value="' + element.quantity + '">\n\
+			<div class="item-price"><span class="price">'+ element.price + '</span><span> €</span></div>\n\
+			<label>quantité :</label><input type="number" class="qty" min="0" value="' + element.quantity + '">\n\
+			<div class="rowPrice"></div>\n\
 			<button class="supp">x</button></li>');
 		}
 	} else {
@@ -142,16 +166,14 @@ function get_number_occurance() {
 }
 
 function get_items_prices() {
-	var listes = get_number_occurance();
-	for (let i = 0; i < userMenu.length; i++) {
-		var theprice = userMenu[i];
-	}
-	for (const key in listes) {
-		var tot = key;
-		var noob =
-			console.log(tot);
-
-
-	}
+	var $listes = $('#user_menu li')
+	$listes.each(function (index) {
+		//	console.log($(this).find('.item-price .price').text());
+		var rowPrice = $(this).find('.item-price .price').text();
+		var rowQuantity = $(this).find('.qty').val();
+		var rowTotalPrice = parseFloat(rowPrice) * parseFloat(rowQuantity);
+		$(this).find('.rowPrice').text("Totale : " + rowTotalPrice + " €");
+		//console.log(rowTotalPrice);
+	})
 
 }
