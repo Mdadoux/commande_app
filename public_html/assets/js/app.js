70f30ad82730f35c9617eaaca33ad15123792d74
mDaroueche <m.daroueche@hotmail.fr>
2019-01-06 09:54:48 +0100

    Rectifications mise en place de nouvelles fonctionnalités + systeme fonctionnelle d'utilisation

--- public_html/assets/js/app.js
+++ public_html/assets/js/app.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
-
+	var $quantity = $('.qty');
 	//$btnAnnulCommand.hide();
 	check_is_commade();
 	get_items_prices();
 
-	//console.log(userMenu);
+	// Afficher les différents igredien 
 	liste_food_items();
-
 	show_user_menu();
 	$("#liste-ingred li").on('click', function () {
-		var aliment = $(this).attr('class');
-		select_item(aliment);
+		var choosedItem = $(this).attr('class');
+		select_item(choosedItem);
 		get_items_prices();
+		update_total_price();
 
 	});
 	//récuperer l'éllément à supprimer 
@@ -20,6 +20,7 @@ $(document).ready(function () {
 		//console.log(itemToSuppr);
 		remove_item_from_menu(itemToSuppr);
 		get_items_prices();
+		update_total_price();
 	});
 
 	//annuler une commande 
@@ -30,8 +31,6 @@ $(document).ready(function () {
 		}
 	})
 
-	var $quantity = $('.qty');
-
 	$quantity.on('change', function () {
 		get_items_prices();
 
@@ -40,10 +39,6 @@ $(document).ready(function () {
 			$(this).value = 0;
 		}
 	})
-
-
-
-
 
 });
 
@@ -60,6 +55,7 @@ function check_is_commade() {
 function add_items_to_menu(item) {
 	userMenu.push(item);
 	localStorage.setItem('userMenu', JSON.stringify(userMenu));
+	get_number_occurance();
 	show_user_menu();
 	check_is_commade();
 
@@ -69,15 +65,54 @@ function add_items_to_menu(item) {
  * @param {le nom de l'element à supprimer } item 
  */
 //suprimer un élement du menu 
+/*
 function remove_item_from_menu(item) {
-	for (let i = 0; i < userMenu.length; i++) {
-		if (userMenu[i].nom == item) {
-			userMenu.splice(userMenu[i], 1);
-			localStorage.setItem('userMenu', JSON.stringify(userMenu));
+	// var foo = ['a', 'b', 'c'];
+	foo = get_number_occurance();
+	console.log(foo);
+
+	for (let index = 0; index < foo.length; index++) {
+		const element = foo[index];
+		console.log(element);
+
+	}
+
+	let index = 0;
+	for (let i in foo) {
+		if (foo[i].nom === item) {
+			var lala = foo[i].nom;
+			var toto = Object.keys(foo);
+			console.log(foo.indexOf(toto['co-cacola']));
+
+			index++;
+			//localStorage.setItem('userMenu', JSON.stringify(userMenu));
 		}
 		show_user_menu();
 		check_is_commade();
+		update_total_price();
 	}
+	
+	
+}
+*/
+
+
+function remove_item_from_menu() {
+	var $listeMenu = $("#user_menu li");
+	$listeMenu.each(function (index) {
+		const quant = $(this).find('.qty').val();
+
+		if (quant >= 2) {
+			console.log(quant);
+			$(this).find('.qty').val(quant - 1);
+
+		} else {
+			$(this).remove();
+		}
+
+	})
+
+	console.log($listeMenu);
 
 
 }
@@ -85,12 +120,22 @@ function remove_item_from_menu(item) {
 //listeer les composants menu 
 function liste_food_items() {
 	for (var i = 0; i < foodItems.length; i++) {
-		var children = foodItems[i];
+		var item = foodItems[i];
 		var $listeIngred = $('#liste-ingred');
-		$listeIngred.append('<li class="' + children.nom + '">' + children.nom + '<div>' + children.price + '</div></li>');
+		$listeIngred.append(`
+		<li class="${item.nom}">
+			<div class="item-img-wrapp">
+				<img src="${item.image}" class="img-responsive">
+			</div>
+			<p class="item-title">${item.nom}</p>
+			<div class="item-price"><span>${item.price}</span>€</div>
+			<button>Choisir</button>		
+		</li>
+		`);
 	}
 }
 
+/*
 function select_item(item) {
 	for (let i = 0; i < foodItems.length; i++) {
 		if (foodItems[i].nom === item) {
@@ -101,27 +146,28 @@ function select_item(item) {
 
 	}
 }
-
+*/
 //afficher le menu si au moioins un item
-
+/*
 function show_user_menu() {
 	var $listeMenu = $("#user_menu");
-
 	var shopping_cart = get_number_occurance();
-	//var propName = JSON.parse(localStorage.getItem('000'));
-	var prop = Object.values(shopping_cart);
-	//console.log(prop);
-	if (userMenu.length > 0) {
+	var prop = JSON.parse(localStorage.getItem('userMenu'));
+	//var prop = Object.values(shopping_cart);
+	update_total_price();
+	console.log(prop);
+	if (prop.length > 0) {
 		$listeMenu.show();
-
 		$listeMenu.empty();
 		for (var i in prop) {
 			const element = prop[i];
 			$listeMenu.append('<li id="' + element.nom + '" class="user_menu-item">\n\
 			<img src="'+ element.image + '"><div class="item-content">' + element.nom + '</div>\n\
-			<div class="item-price"><span class="price">'+ element.price + '</span><span> €</span></div>\n\
+			<div class="item-price">\n\
+				<span class="price">'+ element.price + '</span>\n\
+				<span> €</span></div>\n\
 			<label>quantité :</label><input type="number" class="qty" min="0" value="' + element.quantity + '">\n\
-			<div class="rowPrice"></div>\n\
+			<div class="priceWrapper"><span class="rowPrice"></span> €</div>\n\
 			<button class="supp">x</button></li>');
 		}
 	} else {
@@ -132,7 +178,7 @@ function show_user_menu() {
 
 
 }
-
+*/
 
 function cancel_commande() {
 	localStorage.removeItem('userMenu');
@@ -142,10 +188,10 @@ function cancel_commande() {
 
 function get_number_occurance() {
 	let donnes = JSON.parse(localStorage.getItem('userMenu'));
-	var cart = {};
-	var datas = [];
+
+	// var datas = [];
 	//console.log(liste_of_item);
-	var occurances = {};
+	var datas = {};
 	var counter = 1;
 	for (let i = 0; i < donnes.length; i++) {
 		var element = donnes[i];
@@ -159,6 +205,8 @@ function get_number_occurance() {
 
 		//liste_of_item.push(datas[element.nom]);
 	}
+	//console.log(datas);
+
 	return datas;
 
 	//console.log(occurances);
@@ -172,8 +220,24 @@ function get_items_prices() {
 		var rowPrice = $(this).find('.item-price .price').text();
 		var rowQuantity = $(this).find('.qty').val();
 		var rowTotalPrice = parseFloat(rowPrice) * parseFloat(rowQuantity);
-		$(this).find('.rowPrice').text("Totale : " + rowTotalPrice + " €");
+		$(this).find('.rowPrice').text(rowTotalPrice);
 		//console.log(rowTotalPrice);
 	})
 
 }
+
+function update_total_price() {
+	var $totale_price = $('#total_price');
+	var total_price_value = 0;
+	$totale_price.text();
+	var $prices = $('#user_menu li');
+
+	for (let index = 0; index < $prices.length; index++) {
+		const element = $prices[index];
+		var line_price = $(element).find('.rowPrice').text();
+		total_price_value += parseFloat(line_price);
+	}
+
+	$totale_price.text(total_price_value);
+}
+
--- public_html/assets/js/foodItems.js
+++ public_html/assets/js/foodItems.js
@@ -5,7 +5,7 @@ var foodItems =
 			nom: "steak",
 			type: "viande",
 			price: 2.5,
-			image: "https://p1.storage.canalblog.com/10/46/949906/87751346_o.jpg"
+			image: "https://d301r8r2i6eqlt.cloudfront.net/wp-content/uploads/2017/04/beef.png"
 		},
 		{
 			nom: "cheese burger",
@@ -14,7 +14,7 @@ var foodItems =
 			image: "https://www.dairyqueen.com/Global/Food/Burger_Original-Cheeseburger_470x500.jpg"
 		},
 		{
-			nom: "cocola",
+			nom: "co-cacola",
 			type: "boisson",
 			price: 1.3,
 			image: "https://www.latestfreestuff.co.uk/wp-content/uploads/2018/01/free-mcdonalds-coca-cola-drink-e1515750985951.png"
@@ -62,4 +62,3 @@ if (localStorage.getItem('userMenu') === null) {
 	var userMenu = JSON.parse(localStorage.getItem('userMenu'));
 
 }
-
